e166f5c2155a789d87dc46c560a5b178329b4282
Nicholas Ng <onenylxus@gmail.com>
2024-05-21 12:06:24 +0800

    Environment setup

---
+++ android/app/src/main/kotlin/com/example/talktune/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.onenylxus.talktune
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/secret_example.dart
@@ -0,0 +1,20 @@
+import 'package:firebase_core/firebase_core.dart' show FirebaseOptions;
+
+class Secret {
+  static const FirebaseOptions firebaseOptionsAndroid = FirebaseOptions(
+    apiKey: '',
+    appId: '',
+    messagingSenderId: '',
+    projectId: '',
+    storageBucket: '',
+  );
+
+  static const FirebaseOptions firebaseOptionsIOS = FirebaseOptions(
+    apiKey: '',
+    appId: '',
+    messagingSenderId: '',
+    projectId: '',
+    storageBucket: '',
+    iosBundleId: '',
+  );
+}
